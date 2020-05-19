1199046d61e454f46b6b421c69911857c9cdd035
nuruddianto <nuruddianto@gmail.com>
2020-05-19 19:19:35 +0700

    Add solution

--- code/BiggerIsGreater.java
+++ code/BiggerIsGreater.java
@@ -10,117 +10,56 @@ import java.util.Scanner;
  */
 public class BiggerIsGreater {
 
-    private static char[] c;
+    /***
+     * 1. Find longest non increasing suffix 
+     * 2. Get pivot and switch with rigmost successor 
+     * 3. Reverse the suffix
+     **/
+    // Complete the biggerIsGreater function below.
+    static String biggerIsGreater(String w) {
+        int right = w.length() - 1;
+        char[] c = w.toCharArray();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(sc.readLine());
-//        String line = "";
-//        Scanner sc = new Scanner(System.in);
-//        long T = sc.nextInt();
-        for(long t=0; t < T; t++){
-            String s = sc.readLine();
-            c = null;
-            c = s.toCharArray();
-            int firstChar = findFirstChar(s.toCharArray());
-            if(firstChar == -1){
-                System.out.println("no answer");
-            }else{
-                int secondChar = findSecondChar(firstChar, s.toCharArray());
-                if(secondChar == -1){
-                    System.out.println("no answer");
-                }else{
-                    swap(firstChar, secondChar, c);
-                    doQuickSort(firstChar+1, c.length-1, c);
-                    //System.out.print(firstChar+" "+secondChar);
-                    System.out.println(c);
+        for (int r = right; r >= 1; r--) {
+            if (c[r] > c[r - 1]) {
+                int pivot = c[r - 1];
+                for (int i = right; i > r - 1; i--) {
+                    if (c[i] > pivot) {
+
+                        // switch
+                        char tmp = c[i];
+                        c[i] = c[r - 1];
+                        c[r - 1] = tmp;
+
+                        // reverse
+                        int a = r;
+                        int b = c.length - 1;
+
+                        while (a < b) {
+                            tmp = c[a];
+                            c[a] = c[b];
+                            c[b] = tmp;
+                            a++;
+                            b--;
+                        }
+                        return new String(c);
+                    }
                 }
             }
-
         }
+        return "no answer";
     }
 
-    private static int findFirstChar(char[] c){
-        int length = c.length;
-        if(length == 1){
-            return -1;
-        }
-        for(int i = length - 2; i >= 0; i--){
-            if(c[i] < c[i+1]){
-                return i;
-            }
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        return -1;
+        for(int i = 0; i < n; i++) {
+            String s = sc.next();
+            System.out.println(biggerIsGreater(s));
+        }
+        sc.close();
     }
-
-    private static int findSecondChar(int start, char[] c){
-        int length = c.length;
-        if(length == 1){
-            return -1;
-        }
-        char min = 'z';
-        int index = -1;
-        for(int i = start+1 ; i < length; i++){
-            if(c[start] < c[i] && c[i] < min){
-                min = c[i];
-                index = i;
-            }
-        }
-        return index;
-    }
-
-    private static void swap(int a, int b, char[] s){
-        char tmp = s[a];
-        s[a] = s[b];
-        s[b] = tmp;
-    }
-
-    private static void doQuickSort(int l, int r, char[] s){
-        if( l >= r ){
-            return;
-        }
-
-        int mid = (l + r)/2;
-
-        int left = l;
-        int right = r;
-
-        while(left <= right){
-
-            while(s[left] < s[mid]){
-                left++;
-            }
-
-            while(s[right] > s[mid]){
-                right--;
-            }
-
-            if(left <= right){
-                char tmp = s[left];
-                s[left] = s[right];
-                s[right] = tmp;
-                left++;
-                right--;
-            }
-        }
-
-        if(left < r){
-            doQuickSort(left, r, s);
-        }
-
-        if(right > l){
-            doQuickSort(l, right, s);
-        }
-    }
-
-    private static void printChar(char[] c){
-        for(int i=0; i < c.length; i++){
-            System.out.print(c[i]);
-        }
-        System.out.println();
-    }
-
 }
 
         /*
--- code/FindTheRunningMedian.java
+++ code/FindTheRunningMedian.java
@@ -1,8 +1,56 @@
 package code;
 
+import java.util.*;
+
 class FindTheRunningMedian {
-    public static void main(String[] args) {
-        System.out.println("Hello Finding");
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+        for(int i=0; i< n; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+        System.out.println(Arrays.toString(runningMedian(a)));
+        sc.close();
+    }
+
+    static double[] runningMedian(int[] a) {
+        Queue<Integer> maxHeap = new PriorityQueue<Integer>(10, Collections.reverseOrder());
+        Queue<Integer> minHeap = new PriorityQueue<>();
+
+        double[] result = new double[a.length];
+
+        maxHeap.add(a[0]);
+        result[0] = new Double(a[0]);
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < maxHeap.peek()) {
+                maxHeap.add(a[i]);
+            } else {
+                minHeap.add(a[i]);
+            }
+
+            // balancing
+            if (maxHeap.size() - minHeap.size() > 1) {
+                minHeap.add(maxHeap.remove());
+            } else if (minHeap.size() - maxHeap.size() > 1) {
+                maxHeap.add(minHeap.remove());
+            }
+
+            double leftMed = new Double(maxHeap.peek());
+            double rightMed = new Double(minHeap.peek());
+
+            if (maxHeap.size() == minHeap.size()) {
+                result[i] = (leftMed + rightMed) / 2;
+                System.out.println(result[i]);
+            } else if (maxHeap.size() > minHeap.size()) {
+                result[i] = leftMed;
+            } else {
+                result[i] = rightMed;
+            }
+        }
+        return result;
     }
 }
 
